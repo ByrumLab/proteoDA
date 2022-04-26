1385c63182468bfec39ea75aeb1cc55fbc5f3693
Tim Thurman <TThurman@uams.edu>
2022-04-26 14:53:07 -0500

    streamline and document make_contrasts

--- R/make_contrasts.R
+++ R/make_contrasts.R
@@ -1,30 +1,66 @@
+#' Prepare limma contrasts matrix
+#'
+#' Create the contrasts matrix, for use in limma. Calls
+#' \code{\link{extract_contrast_groups}} and \code{\link{make_log}} as
+#' subfunctions.
+#'
+#' @param file The path to the contrasts file listing the desired contrasts.
+#'   Must be a .csv, .tsv, or .txt file.
+#' @param design The limma design matrix for the model. In the pipeline, this
+#'   is usually the "design" slot of the list returned by
+#'   \code{\link{make_design}}.
+#'
+#' @return A list with 4 slots: \enumerate{
+#'   \item "contrasts"- The contrast matrix, for use in limma
+#'   \item "contrast.vec"- The vector of contrasts, as read from the input
+#'     contrasts file
+#'   \item "param"- A dataframe of parametes used for calling the function
+#'   \item "stats"- A dataframe giving stats (the total number of contrasts).
+#' }
+#' @export
+#'
+#' @examples
+#' # No examples yet
+#'
 make_contrasts <- function(file = NULL,
-                           design ) {
+                           design) {
 
   param <- stats <- list()
 
+  # TODO: I think we should take away this functionality
+  # Script isn't trackable/reproducible if we're selecting files
+  # through clicking in Rstudio
   if (is.null(file)) {
     file = file.choose()
   }
 
-  ## check that file exists in specified location. if file exits and is csv/tsv/txt
-  file <-gsub("\\./", "", file)
-  file.dir <- dirname(file)
-  file <- file.path(file.dir, match.arg(basename(file), list.files(file.dir), several.ok = F))
+  ## check that the file is a csv, tsv, or text file
   filext <- tools::file_ext(file)
-  filext <- match.arg(filext, c("csv", "txt", "tsv"), several.ok = F)
-  if (filext == "txt" | filext == "tsv") {
-    sep <- "\t"
-  }
-  if (filext == "csv") {
-    sep <- ","
+  if (filext %notin% c("csv","txt","tsv")) {
+    cli::cli_abort(c("Problem with input file",
+                     "x" = "Input file must end in {.file .csv}, {.file .tsv}, or {.file .txt}"))
   }
 
+  ## check that file exists
+  if (!file.exists(file)) {
+    cli::cli_abort(c("Cannot find file.",
+                     "x" = "{.file {file}} does not exist",
+                     "i" = "Did you specify the filepath correctly?"))
+  }
+
+  if (filext == "txt" | filext=="tsv") {sep= "\t"}
+  if (filext == "csv") {sep=","}
+
   ## imports contrast file
-  contrast.vec <- utils::read.csv(file = file,
-                                  sep = sep,
-                                  stringsAsFactors = F,
-                                  header = F)
+  ## TODO: From Duah's experience that one time, when she had a minus sign instead
+  # of an equals sign: might be good
+  # to do more checking of the format of this file
+  # frankly, there shouldn't be any more than one columns:
+  # should probably through an error if there is.
+  contrast.vec <- utils::read.delim(file = file,
+                                    sep = sep,
+                                    stringsAsFactors = F,
+                                    header = F)
   contrast.vec <- contrast.vec[,1]
   contrast.vec <-gsub(" ", "", contrast.vec)
 
@@ -34,18 +70,22 @@ make_contrasts <- function(file = NULL,
   ## if the groups defined in the contrast file do no match the design
   ## then return the imported contrast.vec.
   if (!all(unique(unlist(contrast.grps)) %in% colnames(design))) {
-    message("the contrast file imported successfully, however, some of the groups ",
-            "included in the contrasts do not match the design matrix.",
-            "The contrast matrix will need to be defined manually using the makeContrasts() function.")
-    message("contrasts <- makeContrasts(contrasts=contrast.vec, levels=design)\n",
-            "colnames(contrasts) <- gsub('=.*','',colnames(contrasts))")
-    message("Returning the imported contrast.vec for additional processing ...")
+    invalidGroups <- unique(unlist(contrast.grps))[unique(unlist(contrast.grps)) %notin% colnames(design)]
+
+    cli::cli_inform(cli::col_yellow("Some groups present in contrast file do not match design matrix"))
+    cli::cli_inform("{cli::qty(length(invalidGroups))} Contrast group{?s} {.val {invalidGroups}} not found in design matrix")
+    cli::cli_inform("Returning the imported contrast.vec for additional processing")
+    cli::cli_inform("Define contrasts manually using the {.fn limma::makeContrasts} function, e.g.:")
+    cli::cli_inform(c("{.code contrasts <- makeContrasts(contrasts=contrast.vec, levels=design)}",
+                      "{.code colnames(contrasts) <- gsub('=.*','',colnames(contrasts))}"))
     return(contrast.vec)
   }
   ## define contrasts
   contrasts <- limma::makeContrasts(contrasts = contrast.vec, levels = design)
+  # rename cols to just the name of the contrasts, not the full vector
   colnames(contrasts) <- gsub("=.*", "", colnames(contrasts))
 
+  # Write logs
   param[["file"]] <- file
   stats[["no_contrasts"]] <- ncol(contrasts)
   logs <- make_log(param = param,
@@ -53,37 +93,63 @@ make_contrasts <- function(file = NULL,
                    title = "CONTRASTS",
                    save = TRUE)
 
+  # return data
   list(contrasts = contrasts,
        contrast.vec = contrast.vec,
        param = logs$param,
        stats = logs$stats)
 }
 
-
+#' Extract groups from a vector of contrasts
+#'
+#' @param contrast.vec A vector of contrasts, most likely from reading in
+#'   a contrasts text file
+#'
+#' @return A named list of length equal to the number of contrasts. Each element
+#'   in the list is a character vector of the groups involved in that contrast,
+#'   and the names of the list give the contrast names.
+#'
+#' @export
+#'
+#' @examples
+#' # No examples yet
+#'
 extract_contrast_groups <- function(contrast.vec) {
 
-  contrast.vec <- gsub(" ", "", as.character(contrast.vec)) ## remove blank spaces
-  mat   <- matrix(unlist(strsplit(contrast.vec, "=")), ncol = 2, byrow = TRUE)
-  names <- mat[,1]
+  # Remove blank spaces
+  input <- stringr::str_squish(as.character(contrast.vec))
 
-  vec <- mat[,2]
-  vec <- gsub(" ", "", as.character(vec)) ## removes blank spaces
-  vec <- gsub("\\/+[[:digit:]]", "", as.character(vec))  ## removes division by one digit number (/2)
-  vec <- gsub("\\-+[[:digit:]]", "", as.character(vec)) ## removes subtraction by one digit number (-2)
-  vec <- gsub("\\++[[:digit:]]", "", as.character(vec)) ## removes addition of one digit number (+2)
-  vec <- gsub("\\*+[[:digit:]]", "", as.character(vec)) ## removes multiply by one digit number (*2)
-  vec <- gsub("\\+", "-", as.character(vec)) ## replaces + with - (+ -> -)
-  vec <- gsub("\\(|\\)", "", as.character(vec)) ## removes parentheses ()
-  vec <- gsub("\\/", "-", as.character(vec)) ## replace division sign / with -
-  vec <- gsub(" ", "", as.character(vec)) ## removes blank spaces
-  vec2 <- strsplit(vec, "-")
-  names(vec2) <- names
+  # Split at equal sign, into name of comparison
+  # and definittion of comparison.
+  input_split <- matrix(unlist(stringr::str_split(input, "=")), ncol = 2, byrow = T)
+  contrast_names <- input_split[,1]
+  contrasts <- input_split[,2]
 
-  vec3 <- NULL
-  for (i in 1:length(vec2)) {
-    vec3[[i]] <- unique(vec2[[i]])
+
+  # Basically replicating a lot of the stuff that
+  # is already there, but with stringr. Though I have no idea what some of this is for
+  # TODO: Issue with all the single-digit operation removals is that they
+  # won't remove more digits, if there's an operation by a two-digit number
+  # e.g., /22 will get turned into just "2".
+  # Seems suboptimal, but I'm not really sure what the point of this section is
+  # I've changed the regexes so that they will remove operations by any # of digits.
+  processed_contrasts <-  contrasts %>%
+    stringr::str_remove_all(" ") %>% # remove blank spaces TODO: probably unneeded
+    stringr::str_remove_all("\\/[[:digit:]]+") %>% ## removes division by one digit number (/2)
+    stringr::str_remove_all("\\-[[:digit:]]+") %>% ## removes subtraction by one digit number (-2)
+    stringr::str_remove_all("\\+[[:digit:]]+") %>% ## removes addition of one digit number (+2)
+    stringr::str_remove_all("\\*[[:digit:]]+") %>% ## removes multiplication by one digit number (*2)
+    stringr::str_replace_all("\\+", "-") %>%       ## replaces + with - (+ -> -)
+    stringr::str_remove_all("[[()]]") %>%          ## remove parentheses
+    stringr::str_replace_all("\\/", "-")           ## replaces / with -
+
+  # Split on "-", to get just the groups.
+  tmp_split <- strsplit(processed_contrasts, "-")
+  output_list <- NULL
+  for (i in 1:length(tmp_split)) {
+    output_list[[i]] <- unique(tmp_split[[i]])
   }
-  names(vec3) <- names
-
-  vec3
+  names(output_list) <- contrast_names
+  cli::cli_inform("Contrast file imported successfully.")
+  output_list
 }
