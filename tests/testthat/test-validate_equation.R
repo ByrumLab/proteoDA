0eb15c7b46df0128293fbbfe0f38519352c84c23
Timothy Thurman <tthurman@uams.edu>
2022-12-05 11:30:42 -0600

    add tests for validate_formula

--- tests/testthat/test-validate_equation.R
+++ tests/testthat/test-validate_equation.R
@@ -1,46 +1,108 @@
-# ## NEED TO MOVE THIS OVER TO A TEST
-# # Bunch of equations that should work
-# validate_formula("~ 0 + group + xyz + (1 | hello)")
-# validate_formula("~ group + xyz + (1 | hello)")
-# validate_formula("~ 0 + group*xyz + (1 | hello)")
-# validate_formula("~ group*xyz")
-# validate_formula("~ group*xyz*hello")
-# validate_formula("~ group*xyz*hello - xyz:hello")
-# validate_formula(~ 0 + group + xyz + (1 | hello))
-# validate_formula(~ group + xyz + (1 | hello))
-# validate_formula(~ 0 + group*xyz + (1 | hello))
-# validate_formula(~ group*xyz)
-# validate_formula(~ group*xyz*hello)
-# validate_formula(~ group*xyz*hello - xyz:hello)
-#
-# # Bunch of equations that should fail
-# validate_formula("y ~ 0 + group + xyz + (1 | hello)")
-# validate_formula(y ~ 0 + group + xyz + (1 | hello))
-# validate_formula(0 + group + xyz + (1 | hello))
-# validate_formula("0 + group + xyz + (1 | hello)")
-# validate_formula(~ 0 + group + xyz + (1 | ))
-# validate_formula("~ 0 + group + xyz + (1 | )")
-# validate_formula(~ 0 + group + xyz + ( | ))
-# validate_formula("~ 0 + group + xyz + (| )")
-# validate_formula("~ 0 + group + xyz + (1 || hello)")
-# validate_formula("~ 0 + group + xyz + (1 | hello) + (1 | hello)")
-# validate_formula("~ 0 + group + xyz + 1 | hello + 1 | hello")
-# validate_formula("~ 0 + group + xyz + (1 | hello) + 1 | hello")
-# validate_formula("~ 0 + group + xyz + 1 | hello")
-# validate_formula(~ 0 + group + xyz + (1 || hello))
-# validate_formula(~ 0 + group + xyz + (1 | hello) + (1 | hello))
-# validate_formula(~ 0 + group + xyz + 1 | hello + 1 | hello)
-# validate_formula(~ 0 + group + xyz + (1 | hello ) + 1 | hello)
-# validate_formula(~ 0 + group + xyz + 1 | hello)
-# validate_formula("~ 0 + group + xyz + (hello | hello)")
-# validate_formula(~ 0 + group + xyz + (hello | hello))
-# validate_formula("~ 0 + group + xyz + (1 | hello:group)")
-# validate_formula("~ 0 + group + xyz + (1 | hello*group)")
-# validate_formula("~ 0 + group + xyz + (1 | hello/group)")
-# validate_formula("~ 0 + group + xyz + (1 | hello + group)")
-# validate_formula(~ 0 + group + xyz + (1 | hello:group))
-# validate_formula(~ 0 + group + xyz + (1 | hello*group))
-# validate_formula(~ 0 + group + xyz + (1 | hello/group))
-# validate_formula(~ 0 + group + xyz + (1 | hello + group))
-# validate_formula("~ group + (1 | group )")
-# validate_formula(~ group + (1 | group   ))
+test_that("validate_formula rejects eqns with LHS", {
+  expect_error(
+    validate_formula("y ~ 0 + group + xyz + (1 | hello)"),
+    "right-hand side"
+  )
+  expect_error(
+    validate_formula(y ~ 0 + group + xyz + (1 | hello)),
+    "include a response variable"
+  )
+})
+
+test_that("validate_formula rejects eqns without tilde", {
+  expect_error(
+    validate_formula(0 + group + xyz + (1 | hello)),
+    "~"
+  )
+  expect_error(
+    validate_formula("0 + group + xyz + (1 | hello)"),
+    "~"
+  )
+})
+
+test_that("validate_formula rejects malformed random factors", {
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | )"),
+               "Could not")
+  expect_error(validate_formula("~ 0 + group + xyz + (| )"),
+               "Could not")
+  expect_error(validate_formula("~ 0 + group + xyz + (1 || hello)"),
+               "||")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 || hello)),
+               "||")
+  expect_error(validate_formula("~ 0 + group + xyz + 1 | hello"),
+              "parentheses")
+  expect_error(validate_formula(~ 0 + group + xyz + 1 | hello),
+               "parentheses")
+})
+
+test_that("validate_formula rejects multiple random factors", {
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | hello) + (1 | hello)"),
+               "multiple")
+  expect_error(validate_formula("~ 0 + group + xyz + 1 | hello + 1 | hello"),
+               "multiple")
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | hello) + 1 | hello"),
+               "multiple")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 | hello) + (1 | hello)),
+               "multiple")
+  expect_error(validate_formula(~ 0 + group + xyz + 1 | hello + 1 | hello),
+               "multiple")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 | hello ) + 1 | hello),
+               "multiple")
+})
+
+test_that("validate_formula rejects complex or non-intercept random factors", {
+  expect_error(validate_formula("~ 0 + group + xyz + (hello | hello)"),
+               "only influence the intercept")
+  expect_error(validate_formula(~ 0 + group + xyz + (hello | hello)),
+               "only influence the intercept")
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | hello:group)"),
+               "Multiple effects")
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | hello*group)"),
+               "Multiple effects")
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | hello/group)"),
+               "Multiple effects")
+  expect_error(validate_formula("~ 0 + group + xyz + (1 | hello + group)"),
+               "Multiple effects")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 | hello:group)),
+               "Multiple effects")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 | hello*group)),
+               "Multiple effects")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 | hello/group)),
+               "Multiple effects")
+  expect_error(validate_formula(~ 0 + group + xyz + (1 | hello + group)),
+               "Multiple effects")
+})
+
+test_that("validate_formula rejects random factors that are also fixed factors", {
+  expect_error(validate_formula("~ group + (1 | group )"),
+               "fixed")
+  expect_error(validate_formula(~ group + (1 | group   )),
+               "fixed")
+})
+
+test_that("validate_formula accepts valid eqns", {
+  expect_equal(class(validate_formula("~ 0 + group + xyz + (1 | hello)")),
+              "formula")
+  expect_equal(class(validate_formula("~ group + xyz + (1 | hello)")),
+               "formula")
+  expect_equal(class(validate_formula("~ 0 + group*xyz + (1 | hello)")),
+               "formula")
+  expect_equal(class(validate_formula("~ group*xyz")),
+               "formula")
+  expect_equal(class(validate_formula("~ group*xyz*hello")),
+               "formula")
+  expect_equal(class(validate_formula("~ group*xyz*hello - xyz:hello")),
+               "formula")
+  expect_equal(class(validate_formula(~ 0 + group + xyz + (1 | hello))),
+               "formula")
+  expect_equal(class(validate_formula(~ group + xyz + (1 | hello))),
+               "formula")
+  expect_equal(class(validate_formula(~ 0 + group*xyz + (1 | hello))),
+               "formula")
+  expect_equal(class(validate_formula(~ group*xyz)),
+               "formula")
+  expect_equal(class(validate_formula(~ group*xyz*hello)),
+               "formula")
+  expect_equal(class(validate_formula(~ group*xyz*hello - xyz:hello)),
+               "formula")
+})
