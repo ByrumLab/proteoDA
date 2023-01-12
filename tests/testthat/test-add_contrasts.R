2d2619a16c9c2ccaece6fc7e9220ff0c83bc25ef
Tim Thurman <TThurman@uams.edu>
2023-01-12 16:59:17 -0600

    more work on add_contrasts tests

--- tests/testthat/test-add_contrasts.R
+++ tests/testthat/test-add_contrasts.R
@@ -2,6 +2,74 @@
 # Tests for add_contrasts -------------------------------------------------
 # Main function is tested here, with some subfunctions tested below
 
+test_that("add_contrasts gives useful error when DAList doesn't have a statistical design", {
+  input <- readRDS(test_path("fixtures", "add_design_input.rds"))
+  good_input <- c("Treatment1_vs_Control= Treatment1 - Control",
+                  "Treatment2_vs_Control= Treatment2-Control",
+                  "Treatment2 vs Treatment1=Treatment2-Treatment1")
+
+  expect_error(add_contrasts(input, contrasts_vector = good_input), "not have a statistical design")
+})
+
+
+test_that("add_contrasts gives useful error when both contrasts_vector and contrasts_file are provided", {
+  input <- readRDS(test_path("fixtures", "add_contrasts_input.rds"))
+  good_input <- c("Treatment1_vs_Control= Treatment1 - Control",
+                  "Treatment2_vs_Control= Treatment2-Control",
+                  "Treatment2 vs Treatment1=Treatment2-Treatment1")
+
+  expect_error(
+    add_contrasts(input,
+                  contrasts_vector = good_input,
+                  contrasts_file = "file.csv"), "Must supply either")
+})
+
+test_that("add_contrasts gives useful error when neither contrasts_vector and contrasts_file are provided", {
+  input <- readRDS(test_path("fixtures", "add_contrasts_input.rds"))
+
+  expect_error(
+    add_contrasts(input), "Must supply either")
+
+})
+
+test_that("add_contrasts gives useful error when contrasts_file is the wrong type", {
+  input <- readRDS(test_path("fixtures", "add_contrasts_input.rds"))
+
+  expect_error(
+    add_contrasts(input,
+                  contrasts_file = "file.xxx"), "Input file must end")
+})
+
+test_that("add_contrasts gives useful error when contrasts_file is not found", {
+  input <- readRDS(test_path("fixtures", "add_contrasts_input.rds"))
+
+  expect_error(
+    add_contrasts(input,
+                  contrasts_file = "file.csv"), "does not exist")
+
+})
+
+test_that("add_contrasts gives useful error when contrasts_file has more than one column", {
+
+})
+
+test_that("add_contrasts gives useful error when groups in contrast are not present in the design", {
+
+})
+
+test_that("add_contrasts notifies user when overwriting an existing contrast", {
+
+})
+
+test_that("add_contrasts outputs proper contrast when contrasts_vector is supplied", {
+
+})
+
+test_that("add_contrasts outputs proper contrast when contrasts_file is supplied", {
+
+})
+
+
 
 
 # Tests for validate_contrasts --------------------------------------------
