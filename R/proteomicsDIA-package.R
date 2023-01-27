5e92676c5539c586fd736b5d89406644e1338a06
Tim Thurman <TThurman@uams.edu>
2023-01-27 14:43:38 -0600

    move statmod to Imports

--- R/proteomicsDIA-package.R
+++ R/proteomicsDIA-package.R
@@ -6,3 +6,19 @@
 "_PACKAGE"
 
 NULL
+
+
+#' Dummy function for statmod import
+#'
+#' The limma functions we use need to have statmod installed, but limma
+#' only suggests statmod, it doesn't depend on it. So, users can get halfway
+#' through our pipeline, then have to install statmod to run a core function.
+#' This also causes issues with running vignettes. So, we'll have a dummy
+#' function that includes a statmod import.
+#'
+#' @return NULL
+dummy_statmod <- function() {
+  x <- statmod::vecmat()
+
+  NULL
+}
