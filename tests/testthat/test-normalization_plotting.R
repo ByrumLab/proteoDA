b8bcb4ef143b60f9ae98efd51acb4041d4038f7e
Timothy Thurman <tthurman@uams.edu>
2023-02-03 11:25:41 -0600

    start norm plot testing

---
+++ tests/testthat/test-normalization_plotting.R
@@ -0,0 +1,8 @@
+test_that("eval_pn_metric_for_plot output has proper structure", {
+
+})
+
+
+# At the moment, not going to do testing of the
+# various plotting functions, which should all output ggplot objects
+# and testing all of them for correctness would be tricky.
