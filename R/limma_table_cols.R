cadde7f808cc2c86482dc11b3910559001668ff1
ByrumLab <sbyrum@stjude.org>
2026-02-09 11:23:20 -0600

    add limaa_table_cols so DA_table_cols and stat_cols are defined for tests

---
+++ R/limma_table_cols.R
@@ -0,0 +1,5 @@
+# Define parameters from proteoDA_params.R so checks and test run
+#' @keywords internal
+NULL
+DA_table_cols <- c("uniprot_id","Accession.Number","Protein.Description")  # DIANN
+stat_cols = c("logFC", "P.Value", "adj.P.Val", "movingSDs", "logFC_z_scores", "sig.PVal", "sig.FDR") 
